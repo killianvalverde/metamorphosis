c84d761a70c17694fddc52ab1fdb16d951e76fbf
Killian Valverde <KillianValverde@gmail.com>
2024-05-23 17:55:46 +0200

    src: add forward declarations

---
+++ src/metamorphosis/forward_declarations.hpp
@@ -0,0 +1,45 @@
+/* metamorphosis
+ * Copyright (C) 2018-2024 Killian Valverde.
+ *
+ * This file is part of metamorphosis.
+ *
+ * metamorphosis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * metamorphosis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with metamorphosis. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file        forward_declarations.hpp
+ * @brief       forward_declarations header.
+ * @author      Killian Valverde
+ * @date        2024/05/23
+ */
+ 
+#ifndef METAMORPHOSIS_FORWARD_DECLARATIONS_HPP
+#define METAMORPHOSIS_FORWARD_DECLARATIONS_HPP
+
+
+/**
+ * @brief       Contians all metamorphosis resources.
+ */
+namespace metamorphosis {
+
+
+class program;
+class target_file;
+class target_image;
+
+
+}
+
+
+#endif
